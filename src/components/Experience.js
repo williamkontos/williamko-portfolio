cd86e73a4814f6a0f0dd60f41dd5cab5c65074ec
William Kontos <williamjkontos@gmail.com>
2018-11-28 13:11:22 -0500

    resume page is mobile responsive

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -6,13 +6,13 @@ export default class Experience extends Component {
     return (
       <Grid>
         <Cell col={4}>
-          <p>{this.props.startYear} - {this.props.endYear}</p>
+          <p className="start-year">{this.props.startYear} - {this.props.endYear}</p>
         </Cell>
         <Cell col={8}>
           <h4 className="school-name">{this.props.jobName}</h4>
           <i><h6 className="school-name">{this.props.jobTitle}</h6></i>
           <i><h6 className="school-name">{this.props.location}</h6></i>
-          <p>{this.props.jobDescription}</p>
+          <p className="description">{this.props.jobDescription}</p>
         </Cell>
       </Grid>
     )
